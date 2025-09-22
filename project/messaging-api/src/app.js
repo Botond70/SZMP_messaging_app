9ea2115cac2711ebbae153d93dc785ccb27202cf
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-22 10:38:44 +0200

    struct(api): example get method

---
+++ project/messaging-api/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res) =>{
+    res.send('Test get call');
+});
+
+app.listen(port, ()=>{
+    console.log(`now listening on port:${port}`);
+});
