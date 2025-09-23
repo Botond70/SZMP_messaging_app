8eac5d4b5eae0762ad2bf91f5f56e97fe220827e
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-09-23 20:36:24 +0200

    feat(api): created friend Controller

--- project/messaging-api/src/controllers/friendController.js
+++ project/messaging-api/src/controllers/friendController.js
@@ -2,17 +2,58 @@ const friendService = require('../services/friendService');
 
 const getFriendsByUserId = async (req,res) => {
 
+    const {userId} = req.params;
+    try{
+        const friends = await friendService.getFriendsByUserId(userId);
+        res.status(200).json(friends);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
 }
 
 const createFriendByUserId = async (req,res) => {
-
+    const {sender, recipient} = req.params;
+    try {
+        const newFriend = await friendService.createFriend(sender,recipient);
+        res.status(200).json(newFriend);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({err: "Failed to create Friend request"});
+    }
 }
 
 const updateFriendById = async (req,res) => {
-
+    const {id} = req.params;
+    const {status} = req.params;
+    try{
+        const updated = await friendService.updateFriendStatus(id,status);
+        if(!updated){
+            res.status(404).json({err: "Friend request not found"});
+        }
+        res.status(200).json(updated);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({err: "Failed to update Friend request"});
+    }
 }
 
 const deleteFriendById = async (req,res) => {
+    const {id} = req.params;
+    try {
+        const deleted = await friendService.deleteFriendById(id);
+        if(!deleted){
+            res.status(404).json({err: "Friend request not found"});
+        }
+        res.status(204).send();
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({err: "Failed to delete Friend request"});
+    }
 
 }
 
