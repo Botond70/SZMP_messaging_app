2b98c266284c03fc183bef1c77b45afcbc351935
Botond Bártfai <botond.bartfai@bitninja.io>
2025-09-28 16:30:12 +0200

    Cookie perzisztencia implementálva, a demo szerver fájlai törölve

---
+++ project/client/src/Utils.js
@@ -0,0 +1,18 @@
+export function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    let out = cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
+    document.cookie = out;
+};
+
+export function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+};
